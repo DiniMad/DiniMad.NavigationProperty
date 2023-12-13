4787105614f81fdb61d77904aac3fee5d1ccdf88
Seyed Hossein Hosseini <payoff900@gmail.com>
2023-12-13 21:48:45 +0330

    test: Adding some basic test.

---
+++ DiniMad.NavigationProperty.Test/NavigationProperty.cs
@@ -0,0 +1,38 @@
+﻿namespace DiniMad.NavigationProperty.Test;
+
+public class NavigationProperty
+{
+    [Fact]
+    public void SingleLayerInclude()
+    {
+        var navigation = Root.Navigations.First();
+
+        var expectation = new[]
+        {
+            $"{nameof(Root.First)}",
+        };
+
+        navigation.Includes.Should().BeEquivalentTo(expectation);
+    }
+
+    [Fact]
+    public void MultilayerInclude()
+    {
+        var navigation =
+            Root.Navigations.First(First.Navigations.Second(Second.Navigations.Third1(Third.Navigations.Forth()),
+                                                            Second.Navigations.Forth()),
+                                   First.Navigations.Third2());
+
+        var expectation = new[]
+        {
+            $"{nameof(Root.First)}",
+            $"{nameof(Root.First)}.{nameof(First.Second)}",
+            $"{nameof(Root.First)}.{nameof(First.Second)}.{nameof(Second.Third1)}",
+            $"{nameof(Root.First)}.{nameof(First.Second)}.{nameof(Second.Third1)}.{nameof(Third.Forth)}",
+            $"{nameof(Root.First)}.{nameof(First.Second)}.{nameof(Second.Forth)}",
+            $"{nameof(Root.First)}.{nameof(First.Third2)}"
+        };
+
+        navigation.Includes.Should().BeEquivalentTo(expectation);
+    }
+}
---
+++ DiniMad.NavigationProperty.Test/TableStructures.cs
@@ -0,0 +1,36 @@
+﻿namespace DiniMad.NavigationProperty.Test;
+
+public partial class Root : IWithNavigationProperties
+{
+    public virtual First  First  { get; set; }
+    public virtual Second Second { get; set; }
+    public virtual Third  Third  { get; set; }
+    public virtual Forth  Forth  { get; set; }
+}
+
+public partial class First : IWithNavigationProperties
+{
+    public virtual Second Second  { get; set; }
+    public virtual Second Second2 { get; set; }
+    public virtual Third  Third   { get; set; }
+    public virtual Third  Third2  { get; set; }
+    public virtual Forth  Forth   { get; set; }
+    public virtual Forth  Forth2  { get; set; }
+}
+
+public partial class Second : IWithNavigationProperties
+{
+    public virtual Third Third1 { get; set; }
+    public virtual Third Third2 { get; set; }
+    public virtual Forth Forth  { get; set; }
+}
+
+public partial class Third : IWithNavigationProperties
+{
+    public virtual Forth Forth  { get; set; }
+    public virtual Forth Forth2 { get; set; }
+}
+
+public class Forth
+{
+}
