ba6e53ee2fe5ff49751df116d8e056f49f3ca391
Seyed Hossein Hosseini <payoff900@gmail.com>
2023-12-13 18:25:00 +0330

    chore: Creating the NavigationProperty class and implementing it's create method.

---
+++ DiniMad.NavigationProperty/NavigationProperty.cs
@@ -0,0 +1,16 @@
+﻿namespace DiniMad.NavigationProperty;
+
+public class NavigationProperty<T>
+{
+    public IReadOnlyCollection<string> Includes { get; private set; } = null!;
+
+    protected NavigationProperty()
+    {
+    }
+
+    protected static NavigationProperty<T> Create(string name, params string[] thenIncludes)
+    {
+        var      includes = thenIncludes.Select(then => $"{name}.{then}").Append(name);
+        return new() {Includes = includes.ToArray()};
+    }
+}
